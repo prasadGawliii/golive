d6d1221ee5c3714404b45aea05601f6e0cf0f232
prasadg12345 <prasad@blacksoil.co.in>
2025-03-29 12:23:08 +0530

    Updated

--- src/components/Dashboard/ProductDetails.jsx
+++ src/components/Dashboard/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom"; // ✅ Import Link
+import { useParams, Link } from "react-router-dom"; 
 import axios from "axios";
 import "./ProductDetails.css";
 
